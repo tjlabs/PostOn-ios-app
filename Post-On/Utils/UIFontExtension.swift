1727b1b421b19f719c6e1e05c6b5da3510230181
Leo <dev@tjlabscorp.com>
2025-01-14 17:42:31 +0900

    feat: Utils
    update extensions

---
+++ Post-On/Utils/UIFontExtension.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+extension UIFont {
+    class func lemonRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Lemon-Regular", size: size)!
+    }
+    
+    class func lemonadaRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Lemonada-Regular", size: size)!
+    }
+    
+    class func lemonadaBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Lemonada-Bold", size: size)!
+    }
+    
+    class func lemonadaSemiBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Lemonada-SemiBold", size: size)!
+    }
+    
+    class func lemonadaMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "Lemonada-Medium", size: size)!
+    }
+    
+    class func lemonadaLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "Lemonada-Light", size: size)!
+    }
+}
---
+++ Post-On/View/ProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileView.swift
+//  Post-On
+//
+//  Created by 신동현 on 1/14/25.
+//
+
+import Foundation
