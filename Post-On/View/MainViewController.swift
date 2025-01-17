df837fe2971fb9d7e59f4f0d3f1011727906d4fb
Leo <dev@tjlabscorp.com>
2025-01-17 14:36:03 +0900

    chore: token decoding example
    update

--- Post-On/View/MainViewController.swift
+++ Post-On/View/MainViewController.swift
@@ -54,5 +54,69 @@ class MainViewController: UIViewController {
             self.initialView.removeFromSuperview()
         }
     }
+    
+//    func checkToken() {
+//        let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM3MDkxNzA1LCJpYXQiOjE3MzcwOTE0MDUsImp0aSI6Ijc1ZWE4OGYwODUzZDQzNTBiYWFlMmQ4MGRiOTJkOWExIiwidGVuYW50X2lkIjoiMmEwMjVlMTQtYzI0Ni00YmY0LThhNjItMDdkMTY5NDJkYTYyIn0.M8Hpdf1-RKFkeO_4KauxUq9qd_48_dE5dOVtZeaFVKE"
+//
+//        if let expirationDate = JWTDecoder.getExpirationDate(from: accessToken) {
+//            print("accessToken expires on: \(expirationDate)")
+//        } else {
+//            print("Failed to get expiration date")
+//        }
+//        
+//        let refreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTczNzE3NzgwNSwiaWF0IjoxNzM3MDkxNDA1LCJqdGkiOiIzZmRiZGU1ZTI0MDY0M2U0OGU5OTdjZTA1MjlhMmRjZSIsInRlbmFudF9pZCI6IjJhMDI1ZTE0LWMyNDYtNGJmNC04YTYyLTA3ZDE2OTQyZGE2MiJ9.ij4oDWbvzNbXmlKZpXAiNnF6lBFZyZjwPJEt05OZ3jk"
+//
+//        if let expirationDate = JWTDecoder.getExpirationDate(from: refreshToken) {
+//            print("refreshToken expires on: \(expirationDate)")
+//        } else {
+//            print("Failed to get expiration date")
+//        }
+//    }
 }
 
+
+
+//struct JWTDecoder {
+//    static func decodeJWT(token: String) -> [String: Any]? {
+//        let segments = token.split(separator: ".")
+//        guard segments.count == 3 else {
+//            print("Invalid JWT structure")
+//            return nil
+//        }
+//        
+//        let payloadSegment = segments[1]
+//        
+//        // Base64 decode the payload
+//        var base64 = String(payloadSegment)
+//        base64 = base64.replacingOccurrences(of: "-", with: "+")
+//        base64 = base64.replacingOccurrences(of: "_", with: "/")
+//        while base64.count % 4 != 0 {
+//            base64 += "="
+//        }
+//        
+//        guard let payloadData = Data(base64Encoded: base64) else {
+//            print("Invalid Base64 string")
+//            return nil
+//        }
+//        
+//        do {
+//            if let json = try JSONSerialization.jsonObject(with: payloadData, options: []) as? [String: Any] {
+//                return json
+//            }
+//        } catch {
+//            print("Error decoding JSON: \(error)")
+//        }
+//        
+//        return nil
+//    }
+//
+//    static func getExpirationDate(from token: String) -> Date? {
+//        guard let payload = decodeJWT(token: token),
+//              let exp = payload["exp"] as? TimeInterval else {
+//            print("Expiration claim not found")
+//            return nil
+//        }
+//        
+//        return Date(timeIntervalSince1970: exp)
+//    }
+//}
