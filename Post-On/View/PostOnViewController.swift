a79b760f7869a95e28b2881551ab2e561c094c5f
Leo <dev@tjlabscorp.com>
2025-02-10 13:19:43 +0900

    feat: add vc
    create PostOnViewController

---
+++ Post-On/View/PostOnViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+import SnapKit
+import RxCocoa
+import RxSwift
+import RxRelay
+import Then
+
+class PostOnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
