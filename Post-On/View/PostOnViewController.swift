aa4f992e3c59a306c38db96a0b68163d636e222e
Leo <dev@tjlabscorp.com>
2025-02-10 14:22:34 +0900

    feat: add topView in PostOnVC
    add topView

--- Post-On/View/PostOnViewController.swift
+++ Post-On/View/PostOnViewController.swift
@@ -8,12 +8,51 @@ import Then
 
 class PostOnViewController: UIViewController {
 
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupLayout()
     }
     
-    func goToBack() {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    // MARK: - Layout
+    private func setupLayout() {
+        self.setupTitle()
         
+        view.addSubview(containerView)
+        containerView.snp.makeConstraints { make in
+            make.top.equalTo(topView.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setupTitle() {
+        self.titleLabel.textColor = .white
+        self.titleLabel.font = UIFont.lemonRegular(size: 20)
+        self.titleLabel.layer.shadowOpacity = 0.5
+        self.titleLabel.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+    }
+    
+    // MARK: - Action
+    private func goToBack() {
+        DispatchQueue.main.async {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    @IBAction func tapBackButton(_ sender: UIButton) {
+        goToBack()
     }
 }
